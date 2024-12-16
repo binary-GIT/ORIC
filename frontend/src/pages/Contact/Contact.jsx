0e0a26fa54d9493ee4cef5eec4cd3e97a5135c3e
binary-GIT <168764711+binary-GIT@users.noreply.github.com>
2024-12-16 19:07:43 +0500

    Working on contact

---
+++ frontend/src/pages/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+        <div className="contact-page-container">'
+            <div className="sidebar">
+                
+            </div>
+        </div>
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Contact
